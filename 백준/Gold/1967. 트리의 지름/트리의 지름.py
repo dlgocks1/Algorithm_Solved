b8cfc43015c06443f7318eb2b8568d00c37eeb95
PCBL <cksgodl1@naver.com>
2022-03-18 13:14:32 +0900

    [Gold IV] Title: 트리의 지름, Time: 108 ms, Memory: 46936 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,31 @@
+import sys
+import heapq
+sys.setrecursionlimit(10**9)
+
+n = int(sys.stdin.readline())
+arr = [[] for _ in range(n+1)]
+
+for i in range(n-1):
+    a,b,c = map(int,sys.stdin.readline().split())
+    arr[a].append([b,c])
+    arr[b].append([a,c])
+
+isvisited = [False] * (n+1)
+ans = 0
+
+def dfs(v,dist):
+    isvisited[v] = True
+    for mv,mt in arr[v]:
+        if isvisited[mv] == False:
+            distance[mv] = dist + mt
+            dfs(mv,dist+mt)
+distance = [-1] * (n+1)
+distance[1] = 0
+dfs(1,0)
+
+now = (distance.index(max(distance)))
+distance = [-1] * (n+1)
+distance[now] = 0
+isvisited = [False] * (n+1)
+dfs(now,0)
+print(max(distance))
