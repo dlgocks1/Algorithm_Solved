e44fdf7b2e5f5290eb53baf035af49e490ced406
Falco <cksgodl1@naver.com>
2023-01-02 00:48:02 +0900

    [Silver III] Title: 2×n 타일링, Time: 72 ms, Memory: 32976 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+n = int(sys.stdin.readline())
+dp = [0 for i in range(n+1)]
+if n<=2:
+    print(n)
+    quit()
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+for i in range(3,n+1):
+    dp[i] = (dp[i-1] + dp[i-2]) %10007
+print(dp[n])
