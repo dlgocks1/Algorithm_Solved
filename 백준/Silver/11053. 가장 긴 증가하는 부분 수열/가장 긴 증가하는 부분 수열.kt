732da72c0beec5d4407f7d72b21f8967e04255c4
Falco <cksgodl1@naver.com>
2023-01-03 23:52:36 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 208 ms, Memory: 22500 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    val N = readLine()!!.toInt()
+    val nums = readLine()!!.split(" ").map { it.toInt() }
+    val dp = MutableList(N) { 1 }
+    // 10 20 15 20 30 25 20
+    // 10, 30, 10, 20, 20, 10
+    //  1   1   2   1   1   2
+
+    var mx = 1
+    for (i in nums.indices) {
+        for (j in 0..i) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[j] + 1, dp[i])
+                mx = Math.max(mx, dp[i])
+            }
+        }
+    }
+    println(mx)
+}
