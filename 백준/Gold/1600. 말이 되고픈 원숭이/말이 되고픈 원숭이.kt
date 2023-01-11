202f5ce36d37630ef0b65838fb3f709ed40c4b50
Falco <cksgodl1@naver.com>
2023-01-12 00:23:01 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 752 ms, Memory: 101992 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.kt
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.kt
@@ -1,39 +1,29 @@
-
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.io.*
 import java.util.*
 
 data class Point(var x: Int, var y: Int, var d: Int, var k: Int)
 
 fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val dx = intArrayOf(1, -1, 0, 0) // 4 방향
+    val dy = intArrayOf(0, 0, 1, -1)
+    val horseDx = intArrayOf(-2, -1, 1, 2, -2, -1, 1, 2) // 8방향
+    val horseDy = intArrayOf(-1, -2, -2, -1, 1, 2, 2, 1)
 
-    val dx = arrayOf(0, 0, 1, -1)
-    val dy = arrayOf(1, -1, 0, 0)
-    val hx = arrayOf(-2, -2, 2, 2, 1, -1, 1, -1)
-    val hy = arrayOf(1, -1, 1, -1, 2, 2, -2, -2)
-
-
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val k = br.readLine().toInt()
-    val wh = br.readLine().split(" ")
-
-    val w = wh[0].toInt()
-    val h = wh[1].toInt()
-
-    val map = Array(h) { Array(w) { 0 } }
-
-    repeat(h) { x ->
-        val st = StringTokenizer(br.readLine(), " ")
-        repeat(w) { y -> map[x][y] = st.nextToken().toInt() }
-    }
+    val K = reader.readLine().toInt()
+    val (W, H) = reader.readLine().split(" ").map { it.toInt() }
+    val board = Array(H) { Array(W) { 0 } }
 
     val q: Queue<Point> = LinkedList()
-    val visited = Array(h) { Array(w) { Array(k + 1) { false } } }
+    val isvisited = Array(H) { Array(W) { Array(K + 1) { false } } }
 
-    // 맨 왼쪽 위 -> 맨 오른쪽 아래
+    repeat(H) { y ->
+        val st = StringTokenizer(reader.readLine(), " ")
+        repeat(W) { x -> board[y][x] = st.nextToken().toInt() }
+    }
+
     q.offer(Point(0, 0, 0, 0))
-    visited[0][0][0] = true
-
+    isvisited[0][0][0] = true
     while (q.isNotEmpty()) {
         val target = q.poll()
         val tx = target.x
@@ -41,33 +31,33 @@ fun main() {
         val tk = target.k
         val td = target.d
 
-        if (tx == h - 1 && ty == w - 1) {
+        if (tx == H - 1 && ty == W - 1) {
             println(td)
             return
         }
 
-        // 상하좌우
         for (i in 0 until 4) {
             val nx = tx + dx[i]
             val ny = ty + dy[i]
 
-            if (nx !in 0 until h || ny !in 0 until w || visited[nx][ny][tk] || map[nx][ny] == 1) continue
+            if (nx !in 0 until H || ny !in 0 until W || isvisited[nx][ny][tk] || board[nx][ny] == 1) continue
             q.offer(Point(nx, ny, td + 1, tk))
-            visited[nx][ny][tk] = true
+            isvisited[nx][ny][tk] = true
         }
 
         // knight 처럼 움직임
-        if (tk < k) {
+        if (tk < K) {
             for (i in 0 until 8) {
-                val nx = tx + hx[i]
-                val ny = ty + hy[i]
+                val nx = tx + horseDx[i]
+                val ny = ty + horseDy[i]
                 val nk = tk + 1
 
-                if (nx !in 0 until h || ny !in 0 until w || visited[nx][ny][nk] || map[nx][ny] == 1) continue
+                if (nx !in 0 until H || ny !in 0 until W || isvisited[nx][ny][nk] || board[nx][ny] == 1) continue
                 q.offer(Point(nx, ny, td + 1, nk))
-                visited[nx][ny][nk] = true
+                isvisited[nx][ny][nk] = true
             }
         }
+
     }
 
     println(-1)
