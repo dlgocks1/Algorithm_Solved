d287d1149e9fc0fc75b36fd400d207777c470958
Falco <cksgodl1@naver.com>
2023-02-02 16:50:14 +0900

    [Silver I] Title: 격자상의 경로, Time: 192 ms, Memory: 15640 KB -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.kt
@@ -0,0 +1,33 @@
+
+import java.io.*
+
+private val reader = BufferedReader(InputStreamReader(System.`in`))
+
+fun main() {
+    val (N, M, K) = reader.readLine().split(" ").map { it.toInt() }
+
+    var dp = Array(N) { IntArray(M) { 1 } }
+    // K == 3일 때
+
+    val targetY = if(K == 0) 0 else K / M
+    val targetX = if(K == 0) 0 else K % M - 1
+
+//    println(targetX)
+//    println(targetY)
+    for(y in 1 .. targetY){
+        for(x in 1  .. targetX) {
+            dp[y][x] = dp[y-1][x] + dp[y][x-1]
+        }
+    }
+    val hap1 = dp[targetY][targetX]
+    dp = Array(N) { IntArray(M) { 1 } }
+
+    for(y in targetY+1 until N){
+        for(x in targetX+1 until M) {
+            dp[y][x] = dp[y-1][x] + dp[y][x-1]
+        }
+    }
+    val hap2 = dp[N-1][M-1]
+    println(hap1 * hap2)
+
+}
